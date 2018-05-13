a0235624347f5ef386de4859be6ece63559f9b09
egordunaev <egodunaev@gmail.com>
2018-05-14 00:34:27 +0300

    Меню
    Добавлено главное меню с возможностью запуска игры, настроект и выхода.
    В настройках возможность настройки громкости и разрешения экрана (есть баг при котором игра не выключает полноэкранный режим)
    В игре попробовал сделать движение противника но такое ощущение будто он застревает сам в себе и у меня была пара мыслей как это исправить.

---
+++ Assets/Scripts/OptionsMenu.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class OptionsMenu : MonoBehaviour {
+
+    public Dropdown resolutionDropdown;
+
+    public AudioMixer audioMixer;
+    Resolution[] resolutions;
+    /// <summary>
+    /// При запуске считывает все возможные разрешения экрана и выводит их списком для настроек.
+    /// </summary>
+    private void Start()
+    {
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+
+        int currentResolutionIndex = 0;
+
+        List<string> options = new List<string>();
+        for (int i=0; i<resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+            if(resolutions[i].width==Screen.currentResolution.width && resolutions[i].height==Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+    /// <summary>
+    /// Метод, который устанавливает выбранное разрешение экрана.
+    /// </summary>
+    /// <param name="resolutionIndex">Индекс устанавливаемого разрешения экрана.</param>
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+    /// <summary>
+    /// Управление громкостью.
+    /// </summary>
+    /// <param name="volume">Громкость</param>
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+        Debug.Log("Volume check: " + volume);
+    }
+    /// <summary>
+    /// Метод полноэкранного режима.
+    /// </summary>
+    /// <param name="isFullscreen">Статус полноэкранного режима</param>
+    public void SetFullScreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+}
